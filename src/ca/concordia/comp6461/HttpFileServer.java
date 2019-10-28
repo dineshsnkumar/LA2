b5003a559d48d58fd075d1d2d0432165ad77c49c
dinesh <dineshsnkumar@gmail.com>
2019-10-28 19:32:22 -0400

    GET working listing all the files in dir

--- src/ca/concordia/comp6461/HttpFileServer.java
+++ src/ca/concordia/comp6461/HttpFileServer.java
@@ -10,8 +10,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Date;
+
+/*
+ * References 
+ * 	https://github.com/lakshayverma629/COMP6461LA2/blob/master/src/com/COMP6461/server/SocketConnectionFromServer.java
+ */
 
 public class HttpFileServer {
 
@@ -35,7 +42,29 @@ public class HttpFileServer {
 
 				BufferedReader buffreader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
+				StringBuffer sb = new StringBuffer();
+
 				String line = buffreader.readLine();
+
+				sb.append(line);
+
+				String nextLine = "";
+
+				Integer postDataCheck = -1;
+
+				while ((nextLine = buffreader.readLine()) != null && (nextLine.length()) != 0) {
+
+					if (nextLine.contains("Content-Length:")) {
+
+						postDataCheck = new Integer(
+								nextLine.substring(nextLine.indexOf("Content-Length:") + 16, nextLine.length()))
+										.intValue();
+
+					}
+
+					sb.append(nextLine);
+
+				}
 
 				String[] parsedString = line.split("\\s+");
 
@@ -43,26 +72,44 @@ public class HttpFileServer {
 
 				String fileName = parsedString[1];
 
+				String post = "";
+
+				if (postDataCheck > 0) {
+
+					char[] charArray = new char[postDataCheck];
+
+					buffreader.read(charArray, 0, postDataCheck);
+
+					post = new String(charArray);
+
+				}
+
+				System.out.println(post);
+
+				PrintWriter printWriterOut = new PrintWriter(client.getOutputStream());
+
+				BufferedOutputStream buffOutStream = new BufferedOutputStream(client.getOutputStream());
+
 				if (method.equalsIgnoreCase("GET") && fileName.length() > 1) {
 
-					PrintWriter printWriterOut = new PrintWriter(client.getOutputStream());
-
-					BufferedOutputStream buffOutStream = new BufferedOutputStream(client.getOutputStream());
-
 					int checkFileExists = getFileLength(fileName);
-
+					
+					boolean check = checkIfFileExists(fileName);
+						
 					if (checkFileExists > 0) {
 
 						printWriterOut.println("HTTP/1.1 200 OK");
-						
+
 						printWriterOut.println("Content-length: " + checkFileExists);
 
 					} else {
 
+						System.out.println("File Not found");
+
 						printWriterOut.println("HTTP/1.1 404 FILE NOT FOUND");
-						
+
 						String fileNotFound = "HTTP 404 :File Requested Not found";
-						
+
 						printWriterOut.println("Content-length: " + fileNotFound.length());
 
 					}
@@ -73,8 +120,6 @@ public class HttpFileServer {
 
 					printWriterOut.println("Content-type: " + "text/plain");
 
-					
-
 					printWriterOut.println();
 
 					printWriterOut.flush();
@@ -83,9 +128,58 @@ public class HttpFileServer {
 
 					buffOutStream.flush();
 
+				} else if (method.equalsIgnoreCase("GET")) {
+
+					System.out.println("Invoking GET to list all the files in current directory");
+					
+					StringBuffer fileList= listAllFiles();
+					
+					System.out.println(fileList);
+					
+					printWriterOut.println("HTTP/1.1 200 OK");
+
+					printWriterOut.println("Content-length: " + fileList.length());
+
+					printWriterOut.println("Server: httpfs");
+
+					printWriterOut.println("Date: " + new Date());
+
+					printWriterOut.println("Content-type: " + "text/plain");
+
+					printWriterOut.println();
+
+					printWriterOut.flush();
+
+					buffOutStream.write(fileList.toString().getBytes());
+
+					buffOutStream.flush();
+					
+
 				}
 
 				if (method.equalsIgnoreCase("POST")) {
+
+					System.out.println("POST method invoked");
+
+					int size = createNewFile(fileName, post);
+
+					printWriterOut.println("HTTP/1.1 200 OK");
+
+					printWriterOut.println("Content-length: " + "Successfuly created".length());
+
+					printWriterOut.println("Server: httpfs");
+
+					printWriterOut.println("Date: " + new Date());
+
+					printWriterOut.println("Content-type: " + "text/plain");
+
+					printWriterOut.println();
+
+					printWriterOut.flush();
+
+					buffOutStream.write("Successfuly created \n".getBytes());
+
+					buffOutStream.flush();
 
 				}
 
@@ -98,15 +192,95 @@ public class HttpFileServer {
 
 	}
 
+	public static StringBuffer listAllFiles() {
+
+		StringBuffer filesList = new StringBuffer();
+
+		Path path = Paths.get("");
+
+		String s = path.toAbsolutePath().toString();
+		
+		File file = new File(s);
+		
+		File[] filesInDir = file.listFiles();
+		
+		for(File f : filesInDir) {
+			
+			filesList.append(f.toString());
+			
+			filesList.append("\n");
+			
+		}
+
+		return filesList;
+
+	}
+	
+	
+	
+	public static boolean checkIfFileExists(String fileName) {
+		
+		Path path = Paths.get("");
+
+		String s = path.toAbsolutePath().toString();
+		
+		String completePath = s+fileName;
+		
+		File file = new File(completePath);
+		
+		if(file.exists()) {
+			
+			return true;
+			
+		}
+		
+		return false;
+		
+	}
+	
+	
+	
+
+	public static int createNewFile(String url, String postData) throws IOException {
+
+		String path = Paths.get(".").toAbsolutePath().toString();
+
+		String newFilePath = path + url;
+
+		File file = new File(newFilePath);
+
+		if (!file.exists()) {
+
+			boolean fileCreated = file.createNewFile();
+
+			System.out.println("File created" + fileCreated);
+
+			Files.write(Paths.get(newFilePath), postData.getBytes());
+
+		} else {
+
+			System.out.println("File is already present updating content");
+
+			Files.write(Paths.get(newFilePath), postData.getBytes());
+
+		}
+
+		return (int) file.length();
+
+	}
+
 	public static int getFileLength(String fileName) {
+		
+		Path path = Paths.get("");
 
-		String path = fileName.substring(1);
+		String s = path.toAbsolutePath().toString();
 
-		URL url = HttpFileServer.class.getResource(path);
+		String completePath = s+fileName;
 
-		if (url != null) {
 
-			File file = new File(url.getPath());
+		if (s != null) {
+
+			File file = new File(completePath);
 
 			return (int) file.length();
 
@@ -117,17 +291,19 @@ public class HttpFileServer {
 
 	}
 
-	public static byte[] readContentFromFile(String path) throws IOException {
+	public static byte[] readContentFromFile(String url) throws IOException {
 
-		String file = path.substring(1);
+		String path = Paths.get(".").toAbsolutePath().toString();
 
-		URL url = HttpFileServer.class.getResource(file);
+		String newFilePath = path + url;
 
 		byte[] content = null;
+		
+		File file = new File(newFilePath);
 
-		if (url != null) {
+		if (file.exists()) {
 
-			content = Files.readAllBytes(Paths.get(url.getPath()));
+			content = Files.readAllBytes(Paths.get(newFilePath));
 
 		} else {
 
